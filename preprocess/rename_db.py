655802298d68f550b54c66f5e45bb84a31f89098
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-07-15 15:55:40 +0800

    Update results

--- preprocess/rename_db.py
+++ preprocess/rename_db.py
@@ -6,19 +6,56 @@ from sqlalchemy import create_engine
 db_string = 'postgres://postgres:DLvalue123@hkpolyu.cgqhw7rofrpo.ap-northeast-2.rds.amazonaws.com:5432/postgres'
 engine = create_engine(db_string)
 
-if __name__ == '__main__':
+def new_results_lightgbm():
 
     try:
         result_all = pd.read_csv('result_all.csv')
+        print('local version run - result_all')
 
     except:
+        print('-----------------> downloading data from DB TABLE results_lightgbm')
+
         with engine.connect() as conn:
             result_all = pd.read_sql('SELECT * FROM results_lightgbm', conn)
         engine.dispose()
 
         result_all.to_csv('result_all.csv', index=False)
 
+    print(result_all)
+
     rename = pd.read_excel('preprocess/ratio_calculation.xlsx','DB_name')
-    print(rename)
+
+    # 1. reset trial_lgbm to sequential list
+    last_correct_num = result_all.loc[result_all['name']!='ibes qcut x - new industry', 'trial_lgbm'].to_list()[-1]
+    mask = result_all['name']=='ibes qcut x - new industry'
+
+    wrong_num = result_all.loc[mask, 'trial_lgbm'].to_list()
+    right_num = [x + last_correct_num + 1 for x in wrong_num]
+
+    result_all.loc[mask, 'trial_lgbm'] = result_all.loc[mask, 'trial_lgbm'].replace(wrong_num, right_num)
+    print(result_all.loc[mask, 'trial_lgbm'])
+
+    # 2. rename
+    result_all['name'] = result_all['name'].replace(rename['old_name'].to_list(), rename['new_name'].to_list())
+
+def new_results_lightgbm_stock():
+
+    try:
+        result_all = pd.read_csv('result_all_stock.csv')
+        print('local version run - result_all')
+
+    except:
+        print('-----------------> downloading data from DB TABLE results_lightgbm')
+
+        with engine.connect() as conn:
+            result_all = pd.read_sql('SELECT * FROM result_all_stock', conn)
+        engine.dispose()
+
+        result_all.to_csv('result_all_stock.csv', index=False)
+
+    print(result_all)
+
+if __name__ == '__main__':
+    new_results_lightgbm_stock()
 
 
