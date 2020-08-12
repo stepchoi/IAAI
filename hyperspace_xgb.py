4d82a940b6064143e5782152e0acf83b06f79786
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-08-12 16:39:59 +0800

    update xgboost

--- hyperspace_xgb.py
+++ hyperspace_xgb.py
@@ -4,9 +4,9 @@ space_xgb = {
     'eta': hp.choice('eta', [0.01, 0.1, 0.3]), # remove 0.12
     'booster': hp.choice('booster', ['dart']),
     'max_depth': hp.choice('max_depth',[3, 8, 15]),
-    # 'max_bin': hp.choice('max_bin', [128, 256]),
-    'num_leaves': hp.choice('num_leaves', [125, 250]),  # remove 75
-    'min_child_weight': hp.choice('min_child_weight', [1, 5]), # remove 25, 50
+    'max_bin': hp.choice('max_bin', [128, 256]),
+    'num_leaves': hp.choice('num_leaves', [75, 256, 1024]),  # remove 75
+    'min_child_weight': hp.choice('min_child_weight', [0, 0.1, 1, 5]), # remove 25, 50
     'colsample_bytree': hp.choice('colsample_bytree', [0.9, 1]), # remove 0.7
     'subsample': hp.choice('subsample', [0.8, 0.9]),
     'gamma': hp.choice('gamma', [0.01, 0.1, 0.5]), # remove 0.08
