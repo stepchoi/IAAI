bc0ae45a00453d8a4070ba174b23e92b4e149bfa
loratech <lora0218G>
2020-08-13 15:29:46 +0800

    Update dense

--- hyperspace_dense.py
+++ hyperspace_dense.py
@@ -17,14 +17,14 @@ space_big = {
 
 space_small = {
     'num_Dense_layer': hp.choice('num_Dense_layer', [3, 4, 5]),  # number of layers ONE layer is TRIVIAL # drop 2, 3, 4
-    'learning_rate': hp.choice('lr', [2, 3]),    # => 1e-x - learning rate - REDUCE space later - correlated to batch size
+    'learning_rate': hp.choice('lr', [1, 2]),    # => 1e-x - learning rate - REDUCE space later - correlated to batch size
                                                     # remove lr = 5 & 7 after tuning
     'init_nodes': hp.choice('init_nodes', [8, 16]),  # nodes for Dense first layer -> LESS NODES
-    'dropout': hp.choice('dropout', [0.25, 0.5]),
+    'dropout': hp.choice('dropout', [0.15, 0.25]),
 
     'nodes_mult': hp.choice('nodes_mult', [0, 1]),          # nodes growth rate
     'mult_freq': hp.choice('mult_freq', [2, 3]),         # nodes double frequency
-    'mult_start': hp.choice('mult_start', [2, 3]),       # first layer nodes number growth
+    'mult_start': 2,       # first layer nodes number growth
 
     'activation': hp.choice('activation', ['relu']), # JUST relu for overfitting
     'batch_size': 128, # reduce batch size space # drop 512
